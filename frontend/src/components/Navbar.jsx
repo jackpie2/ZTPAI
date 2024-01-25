881f63bda5c8ef75cb3383f3086ace9666164f8c
jackpie <66798628+jackpie2@users.noreply.github.com>
2024-01-25 20:12:17 +0100

    added mobile support for navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import {
 	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
 	NavigationMenuItem,
 	NavigationMenuLink,
 	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
 	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import {
@@ -15,18 +19,28 @@ import { Coffee } from "lucide-react";
 import { Link } from "react-router-dom";
 import { ModeToggle } from "./ModeToggle";
 import { useNavigate } from "react-router-dom";
+import { Menu, LogIn, UserRound } from "lucide-react";
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
 	const navigate = useNavigate();
 
 	return (
-		<div className="shadow-sm border-b flex w-full justify-start items-center font-mono text-3xl p-3 gap-4">
+		<div className="shadow-sm border-b flex w-full justify-start items-center text-2xl p-3 gap-4">
 			<NavigationMenu className={"w-full max-w-full justify-start"}>
 				<NavigationMenuList>
 					<NavigationMenuItem>
 						<Link
 							to="/"
-							className="flex items-center gap-2 flex-grow"
+							className="flex items-center gap-2 flex-grow font-bold"
 						>
 							<Coffee />
 							CoffeeDB
@@ -38,10 +52,65 @@ export default function Navbar() {
 					<NavigationMenuItem>
 						<ModeToggle />
 					</NavigationMenuItem>
+					<Sheet>
+						<SheetTrigger className="md:hidden w-9 h-9 flex items-center justify-center">
+							<Menu />
+						</SheetTrigger>
+						<SheetContent>
+							<SheetHeader>
+								<SheetTitle>Navigation</SheetTitle>
+							</SheetHeader>
+							<div className="pt-6 flex flex-col gap-2">
+								{localStorage.getItem("token") ? (
+									<>
+										<Button className="w-full">
+											<Link to="/profile">
+												<div className="text-xl flex items-center gap-2">
+													<UserRound />
+													<span>Profile</span>
+												</div>
+											</Link>
+										</Button>
+										<Button className="w-full">
+											<Link
+												onClick={() => {
+													localStorage.removeItem(
+														"token"
+													);
+													localStorage.removeItem(
+														"refresh"
+													);
+													navigate("/");
+												}}
+											>
+												<div className="text-xl flex items-center gap-2">
+													<FontAwesomeIcon
+														icon={
+															faArrowRightFromBracket
+														}
+													/>
+													Logout
+												</div>
+											</Link>
+										</Button>
+									</>
+								) : (
+									<Button className="w-full">
+										<Link to="/login">
+											<div className="text-xl flex items-center gap-2">
+												<LogIn />
+												Login
+											</div>
+										</Link>
+									</Button>
+								)}
+							</div>
+						</SheetContent>
+					</Sheet>
 
 					{localStorage.getItem("token") ? (
 						<>
-							<NavigationMenuItem>
+							<NavigationMenuItem className="md:block hidden">
 								<Link to="/profile">
 									<NavigationMenuLink
 										className={navigationMenuTriggerStyle()}
@@ -53,7 +122,7 @@ export default function Navbar() {
 									</NavigationMenuLink>
 								</Link>
 							</NavigationMenuItem>
-							<NavigationMenuItem>
+							<NavigationMenuItem className="md:block hidden">
 								<Link
 									onClick={() => {
 										localStorage.removeItem("token");
@@ -75,7 +144,7 @@ export default function Navbar() {
 							</NavigationMenuItem>
 						</>
 					) : (
-						<NavigationMenuItem>
+						<NavigationMenuItem className="md:block hidden">
 							<Link to="/login">
 								<NavigationMenuLink
 									className={navigationMenuTriggerStyle()}
